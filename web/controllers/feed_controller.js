4aaf1991fe8510804305588d09c2cc78c876f8e9
Luke <luke.m.breckenridge@gmail.com>
2014-06-15 10:31:33 -0400

    updated view

--- web/controllers/feed_controller.js
+++ web/controllers/feed_controller.js
@@ -2,13 +2,22 @@ var now = new Date();
 var sevenDays = (new Date()).setDate( now.getDate() + 7 );
 var inFifteenMinutes = (new Date()).setMinutes( now.getMinutes() + 15);
 
+var img = new Image();
+var div = document.getElementById('foo');
+
+img.onload = function() {
+  div.appendChild(img);
+};
+
+img.src = 'http://i.imgur.com/7zSbsIo.png';
+
 var testingItems =  [
     {
 	business: {
-	    name: "Ping Pong's Ding Dong Dinner",
+	    name: "Willy Wong-a's Dinner",
 	},
 	offer: {
-	    description: "15% off won tons"
+	    description: "15% off poodle pops"
 	},
 	piggyback: {
 	    expiresAt: sevenDays
